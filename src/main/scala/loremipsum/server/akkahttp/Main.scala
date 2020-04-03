e91e7ac5d8d2bd95b4388407bbfed226a1bf958e
Crosson David <crosson.david@gmail.com>
2020-04-03 22:02:40 +0200

    Overall enhancements, add source code banner, add first unit tests, refactor configuration to add a config name 'prefix'

--- src/main/scala/loremipsum/server/akkahttp/Main.scala
+++ src/main/scala/loremipsum/server/akkahttp/Main.scala
@@ -1,35 +1,29 @@
+/*
+ * Copyright 2020 David Crosson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package loremipsum.server.akkahttp
 
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import loremipsum.server.akkahttp.routing.{AdminRouting, AssetsRouting, ContentRouting}
 import org.slf4j.Logger
 
-class ServiceStarter(dependencies: ServiceDependencies) {
-  import dependencies.config
-
-  val routing: Route = List(
-    AssetsRouting(dependencies),
-    AdminRouting(dependencies),
-    ContentRouting(dependencies),
-  ).map(_.routes).reduce(_ ~ _)
-
-  val prefixedRouting: Route = config.site.cleanedPrefix.map{p => pathPrefix(p) { routing }}.getOrElse(routing)
-
-  val service = Service(
-    name = config.application.code,
-    interface = config.http.listeningInterface,
-    port = config.http.listeningPort,
-    routing = prefixedRouting
-  )
-}
-
 object Main {
-  val logger: Logger = org.slf4j.LoggerFactory.getLogger("Main")
+  val logger: Logger = org.slf4j.LoggerFactory.getLogger("LoremIpsumMain")
 
   def main(args: Array[String]): Unit = {
-    logger.info(s"Application starting")
+    logger.info(s"LoremIpsum application is starting")
     val dependencies = ServiceDependencies.defaults
-    new ServiceStarter(dependencies)
+    val serviceRoutes = ServiceRoutes(dependencies)
+    Service(dependencies, serviceRoutes)
   }
 }
