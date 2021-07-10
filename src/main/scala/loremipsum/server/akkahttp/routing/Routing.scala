c81bb2102db2d681f8ca299d80e49409c2a7ce8e
Crosson David <crosson.david@gmail.com>
2021-07-10 09:07:51 +0200

    Enhance client cache config

--- src/main/scala/loremipsum/server/akkahttp/routing/Routing.scala
+++ src/main/scala/loremipsum/server/akkahttp/routing/Routing.scala
@@ -17,13 +17,14 @@ package loremipsum.server.akkahttp.routing
 
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model.HttpHeader
-import akka.http.scaladsl.model.headers.CacheDirectives.{`max-age`, `must-revalidate`, `no-cache`, `no-store`}
+import akka.http.scaladsl.model.headers.CacheDirectives.{`max-age`, `must-revalidate`, `no-cache`, `no-store`, `proxy-revalidate`}
 import akka.http.scaladsl.model.headers.`Cache-Control`
 import loremipsum.server.akkahttp.tools.JsonImplicits
 
 trait Routing extends JsonImplicits {
   def routes: Route
 
-  val noClientCacheHeaders: List[HttpHeader] = List(`Cache-Control`(`no-cache`, `no-store`, `max-age`(0), `must-revalidate`))
-  val clientCacheHeaders: List[HttpHeader] = List(`Cache-Control`(`max-age`(86400)))
+  val noClientCacheHeaders: List[HttpHeader] = List(`Cache-Control`(`no-store`))
+
+  val clientCacheHeaders: List[HttpHeader] = List(`Cache-Control`(`max-age`(3600), `must-revalidate`, `proxy-revalidate`))
 }
